00706d55c4b77ecceec1d7347ec7c26103a5dccb
Q.P.Liu <qpliu@yahoo.com>
2012-04-01 19:38:11 -0700

    Fix version reset when pruning expired subtries in Haskell implementation.

--- hs/TrieCache.hs
+++ hs/TrieCache.hs
@@ -11,7 +11,7 @@ import Control.Concurrent.STM(STM,TArray,TVar,atomically,newTVar,readTVar,writeT
 import Data.Array.MArray(MArray(newArray),readArray,writeArray,getAssocs,getElems)
 import Data.ByteString(ByteString,index)
 import qualified Data.ByteString as B
-import Data.Maybe(catMaybes)
+import Data.Either(lefts,rights)
 import Data.Word(Word8,Word64)
 
 import Cache(
@@ -20,9 +20,6 @@ import Cache(
     Expires(Expires),Timestamp(Timestamp),Version(Version))
 
 -- | TrieCache.  t is the timestamp type.  e is the type of the cached values.
--- One flaw is that version numbers are reset when expired subtries are
--- pruned.  One fix would be to add a start version to CacheEntry that
--- gets updated every time its subtries are pruned.
 newtype TrieCache t e = TrieCache (TVar (CacheEntry t e))
 
 type Entry t e = Maybe (t,e)
@@ -30,20 +27,20 @@ type Entry t e = Maybe (t,e)
 data CacheEntry t e = CacheEntry {
     ceEntry :: Entry t e,
     ceVersion :: Version,
-    ceTrie :: TArray Word8 (Maybe (CacheEntry t e))
+    ceTrie :: TArray Word8 (Either Version (CacheEntry t e))
     }
 
 -- | Create a new empty TrieCache.
 newTrieCache :: Ord t => IO (TrieCache t e)
 newTrieCache = atomically $ do
-    ce <- newCacheEntry
+    ce <- newCacheEntry (Version 0)
     cache <- newTVar ce
     return (TrieCache cache)
 
-newCacheEntry :: Ord t => STM (CacheEntry t e)
-newCacheEntry = do
-    trie <- newArray (minBound,maxBound) Nothing
-    return CacheEntry { ceEntry = Nothing, ceVersion = Version 0, ceTrie = trie }
+newCacheEntry :: Ord t => Version -> STM (CacheEntry t e)
+newCacheEntry startVersion = do
+    trie <- newArray (minBound,maxBound) (Left startVersion)
+    return CacheEntry { ceEntry = Nothing, ceVersion = startVersion, ceTrie = trie }
 
 expired :: Ord t => CacheEntry t e -> t -> Bool
 expired ce timestamp = maybe False ((timestamp >=) . fst) (ceEntry ce)
@@ -61,27 +58,27 @@ instance Cache TrieCache where
       where
         get' i ce
             | i < B.length key = do
-                maybeCe <- readArray (ceTrie ce) (key `index` i)
-                maybe (return (Left NotFound)) (get' (i + 1)) maybeCe
+                subCe <- readArray (ceTrie ce) (key `index` i)
+                either (const (return (Left NotFound))) (get' (i + 1)) subCe
             | noEntry ce t = return (Left NotFound)
             | otherwise =
                 let Just (_,e) = ceEntry ce in return (Right (e, ceVersion ce))
 
     set (TrieCache cache) key e (Expires exp) (Timestamp t) = atomically $ do
         ce <- readTVar cache
-        (maybeCe,result) <- set' 0 (Just ce)
-        maybe (return ()) (writeTVar cache) maybeCe
+        (updateCe,result) <- set' 0 (Right ce)
+        maybe (return ()) (writeTVar cache) updateCe
         return result
       where
-        set' i Nothing = do
-            ce <- newCacheEntry
-            (maybeCe,result) <- set' i (Just ce)
-            return (maybe (Just ce) Just maybeCe,result)
-        set' i (Just ce)
+        set' i (Left startVersion) = do
+            ce <- newCacheEntry startVersion
+            (updateCe,result) <- set' i (Right ce)
+            return (maybe (Just ce) Just updateCe,result)
+        set' i (Right ce)
             | i < B.length key = do
-                maybeCe <- readArray (ceTrie ce) (key `index` i)
-                (maybeCe',result) <- set' (i + 1) maybeCe
-                maybe (return ()) (writeArray (ceTrie ce) (key `index` i) . Just) maybeCe'
+                subCe <- readArray (ceTrie ce) (key `index` i)
+                (updateCe,result) <- set' (i + 1) subCe
+                maybe (return ()) (writeArray (ceTrie ce) (key `index` i) . Right) updateCe
                 return (Nothing,result)
             | otherwise = do
                 let version = incVersion ce
@@ -89,19 +86,19 @@ instance Cache TrieCache where
 
     add (TrieCache cache) key e (Expires exp) (Timestamp t) = atomically $ do
         ce <- readTVar cache
-        (maybeCe,result) <- add' 0 (Just ce)
-        maybe (return ()) (writeTVar cache) maybeCe
+        (updateCe,result) <- add' 0 (Right ce)
+        maybe (return ()) (writeTVar cache) updateCe
         return result
       where
-        add' i Nothing = do
-            ce <- newCacheEntry
-            (maybeCe,result) <- add' i (Just ce)
-            return (maybe (Just ce) Just maybeCe,result)
-        add' i (Just ce)
+        add' i (Left startVersion) = do
+            ce <- newCacheEntry startVersion
+            (updateCe,result) <- add' i (Right ce)
+            return (maybe (Just ce) Just updateCe,result)
+        add' i (Right ce)
             | i < B.length key = do
-                maybeCe <- readArray (ceTrie ce) (key `index` i)
-                (maybeCe',result) <- add' (i + 1) maybeCe
-                maybe (return ()) (writeArray (ceTrie ce) (key `index` i) . Just) maybeCe'
+                subCe <- readArray (ceTrie ce) (key `index` i)
+                (updateCe,result) <- add' (i + 1) subCe
+                maybe (return ()) (writeArray (ceTrie ce) (key `index` i) . Right) updateCe
                 return (if expired ce t then Just ce { ceEntry = Nothing } else Nothing,result)
             | noEntry ce t =
                 let version = incVersion ce
@@ -110,15 +107,15 @@ instance Cache TrieCache where
 
     replace (TrieCache cache) key version e (Expires exp) (Timestamp t) = atomically $ do
         ce <- readTVar cache
-        (maybeCe,result) <- replace' 0 ce
-        maybe (return ()) (writeTVar cache) maybeCe
+        (updateCe,result) <- replace' 0 ce
+        maybe (return ()) (writeTVar cache) updateCe
         return result
       where
         replace' i ce
           | i < B.length key = do
-                maybeCe <- readArray (ceTrie ce) (key `index` i)
-                (maybeCe',result) <- maybe (return (Nothing,Left NotFound)) (replace' (i + 1)) maybeCe
-                maybe (return ()) (writeArray (ceTrie ce) (key `index` i) . Just) maybeCe'
+                subCe <- readArray (ceTrie ce) (key `index` i)
+                (updateCe,result) <- either (const (return (Nothing,Left NotFound))) (replace' (i + 1)) subCe
+                maybe (return ()) (writeArray (ceTrie ce) (key `index` i) . Right) updateCe
                 return (if expired ce t then Just ce { ceEntry = Nothing } else Nothing,result)
           | noEntry ce t = return (Nothing,Left NotFound)
           | version /= ceVersion ce = return (Nothing,Left VersionMismatch)
@@ -128,15 +125,15 @@ instance Cache TrieCache where
 
     delete (TrieCache cache) key version (Timestamp t) = atomically $ do
         ce <- readTVar cache
-        (maybeCe,result) <- delete' 0 ce
-        maybe (return ()) (writeTVar cache) maybeCe
+        (updateCe,result) <- delete' 0 ce
+        maybe (return ()) (writeTVar cache) updateCe
         return result
       where
         delete' i ce
           | i < B.length key = do
-                maybeCe <- readArray (ceTrie ce) (key `index` i)
-                (maybeCe',result) <- maybe (return (Nothing,Just NotFound)) (delete' (i + 1)) maybeCe
-                maybe (return ()) (writeArray (ceTrie ce) (key `index` i) . Just) maybeCe'
+                subCe <- readArray (ceTrie ce) (key `index` i)
+                (updateCe,result) <- either (const (return (Nothing,Just NotFound))) (delete' (i + 1)) subCe
+                maybe (return ()) (writeArray (ceTrie ce) (key `index` i) . Right) updateCe
                 return (Nothing,result)
           | expired ce t =
                 return (Just ce { ceEntry = Nothing }, Just NotFound)
@@ -146,15 +143,15 @@ instance Cache TrieCache where
 
     touch (TrieCache cache) key (Expires exp) = atomically $ do
         ce <- readTVar cache
-        (maybeCe,result) <- touch' 0 ce
-        maybe (return ()) (writeTVar cache) maybeCe
+        (updateCe,result) <- touch' 0 ce
+        maybe (return ()) (writeTVar cache) updateCe
         return result
       where
         touch' i ce
           | i < B.length key = do
-                maybeCe <- readArray (ceTrie ce) (key `index` i)
-                (maybeCe',result) <- maybe (return (Nothing,Left NotFound)) (touch' (i + 1)) maybeCe
-                maybe (return ()) (writeArray (ceTrie ce) (key `index` i) . Just) maybeCe'
+                subCe <- readArray (ceTrie ce) (key `index` i)
+                (updateCe,result) <- either (const (return (Nothing,Left NotFound))) (touch' (i + 1)) subCe
+                maybe (return ()) (writeArray (ceTrie ce) (key `index` i) . Right) updateCe
                 return (Nothing,result)
           | otherwise =
                 maybe (return (Nothing,Left NotFound))
@@ -172,22 +169,24 @@ instance Cache TrieCache where
       where
         expire' ce = mapM_ (expire'' ce) [minBound .. maxBound]
         expire'' ce i = do
-            maybeCe <- atomically $ readArray (ceTrie ce) i
-            maybe (return ()) expire' maybeCe
+            subCe <- atomically $ readArray (ceTrie ce) i
+            either (const (return ())) expire' subCe
             atomically $ do
-                maybeCe <- readArray (ceTrie ce) i
-                maybeMaybeCe <- maybe (return Nothing) prune maybeCe
-                maybe (return ()) (writeArray (ceTrie ce) i) maybeMaybeCe
+                subCe <- readArray (ceTrie ce) i
+                maybeUpdateCe <- either (const (return Nothing)) prune subCe
+                maybe (return ()) (writeArray (ceTrie ce) i) maybeUpdateCe
         prune ce
           | expired ce t = do
-                leaf <- isLeaf ce
-                if leaf
-                    then return (Just Nothing)
-                    else return (Just (Just ce { ceEntry = Nothing }))
+                subCes <- getElems (ceTrie ce)
+                if null (rights subCes)
+                    then return (Just (Left (foldl maxVersion (ceVersion ce) (lefts subCes))))
+                    else return (Just (Right ce { ceEntry = Nothing }))
           | noEntry ce t = do
-                leaf <- isLeaf ce
-                if leaf then return (Just Nothing) else return Nothing
+                subCes <- getElems (ceTrie ce)
+                if null (rights subCes)
+                    then return (Just (Left (foldl maxVersion (ceVersion ce) (lefts subCes))))
+                    else return Nothing
           | otherwise = return Nothing
-        isLeaf ce = do
-            maybeCes <- getElems (ceTrie ce)
-            return (null (catMaybes maybeCes))
+        maxVersion version1@(Version v1) version2@(Version v2)
+          | v1 > v2 = version1
+          | otherwise = version2
